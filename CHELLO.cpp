bb2c309f6177972d4303ce4088006ce1f84ec657
Phạm Mạnh Phúc <149230687+PhamManhPhuc@users.noreply.github.com>
2024-01-05 12:28:13 +0700

    Add files via upload

---
+++ CHELLO.cpp
@@ -0,0 +1,13 @@
+// Created by PHẠM MẠNH PHÚC
+// https://code.ptit.edu.vn/student/question/CHELLO
+// Hello World
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"Hello PTIT.";
+    system("pause");
+    return 0;
+}
